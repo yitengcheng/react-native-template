dacd0cd55e7e367d76ac72c945c96628e2aba841
fanzehua <316679581@qq.com>
2022-01-19 16:48:03 +0800

    初始化项目

---
+++ ios/Pods/Target Support Files/Pods-react_native_template-react_native_templateTests/Pods-react_native_template-react_native_templateTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_react_native_template_react_native_templateTests : NSObject
+@end
+@implementation PodsDummy_Pods_react_native_template_react_native_templateTests
+@end
